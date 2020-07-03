97c33ce0b5b2c51cb7e8d832b0cfc3a5861ef861
guruck <guruck@gmail.com>
2020-07-02 21:40:03 -0300

    "commit inicial"

---
+++ src/app.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import winston from 'winston';
+// import mongoose from 'mongoose';
+
+const { combine, timestamp, label, printf } = winston.format;
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  return `${timestamp} [${label}] ${level}: ${message}`;
+});
+
+global.logger = winston.createLogger({
+  level: 'silly',
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: 'my-bank-api.log' }),
+  ],
+  format: combine(label({ label: 'my-bank-api.log' }), timestamp(), myFormat),
+});
+
+const app = express();
+
+app.use(express.json());
+app.listen(3000, async () => {
+  logger.info('App inicializado');
+});
